ab0c80aacfad39acdf0850cbe0062c56af60aa1e
Kimseongwoo <100755199+Gukdoli@users.noreply.github.com>
2024-02-22 17:47:30 +0900

    카드 뭉치.py

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,15 +1,13 @@
 def solution(cards1, cards2, goal):
-    
-    answer = 'Yes'
+    answer = 'Yes' 
     for word in goal:
-        if 0 < len(cards1) and cards1[0] == word:
+        if 0 < len(cards1) and cards1[0] == word: 
             cards1.pop(0)
-
         elif 0 < len(cards2) and cards2[0] == word:
             cards2.pop(0)
         else:
             answer = 'No'
             break
-        if len(cards1) == 0 and len(cards2) == 0:
+        if len(cards1) and len(cards2) == 0:
             answer = 'Yes'
     return answer
