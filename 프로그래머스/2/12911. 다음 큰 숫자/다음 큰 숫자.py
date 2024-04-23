eda97fd89b5851f267198ab02eb436b153b01f28
Kimseongwoo <100755199+Gukdoli@users.noreply.github.com>
2024-04-23 15:25:09 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,9 +1,8 @@
 def solution(n):
     b=bin(n)
     a = ''
-    for i in range(n,1000001):
+    for i in range(n+1,1000001):
         a = bin(i)
-        if i != n:
-            if a.count('1') == b.count('1'):
-                return i
+        if a.count('1') == b.count('1'):
+            return i
     return b
